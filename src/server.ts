fb35241d856634bd7120374b6f8156d3e105130d
zebbeh <haggblca@arcada.fi>
2024-02-13 15:48:22 +0200

    added swagger support and loaded petstore

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import register from "./routes/register";
 import db from "./db/db";
 import { sql } from "drizzle-orm";
 import login from "./routes/login";
+import swagger from "./routes/swagger"
+
 
 const query = sql`select "hello world" as text`;
 const result = db.get<{ text: string }>(query);
@@ -10,6 +12,8 @@ console.log(result);
 
 const app = new Hono();
 
+
+app.route("/swagger", swagger)
 app.route("/register", register);
 app.route("/login", login);
 
