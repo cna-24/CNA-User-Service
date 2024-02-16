52f73409ffa3d4626b122f8c7af54b354860d88f
ErikMansen <mansen.erik@gmail.com>
2024-02-16 22:02:11 +0200

    Added docker-compose file for super easy api usage

--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,16 @@ import login from "./routes/login";
 import swagger from "./routes/swagger"
 import user from "./routes/user";
 
+if (!Bun.env.JWT_SECRET) {
+    console.error("Error JWT_SECRET is not set")
+    process.exit(0)
+}
 
 const query = sql`select "hello world" as text`;
 const result = db.get<{ text: string }>(query);
 console.log(result);
 
 const app = new Hono();
-
 
 app.route("/swagger", swagger)
 app.route("/register", register);
