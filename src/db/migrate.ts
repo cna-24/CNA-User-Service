a703eb2dd9c053292110737876123783df1be0ac
ErikMansen <mansen.erik@gmail.com>
2024-02-20 00:22:29 +0200

    I'm all out of funny commit messages I just tried changing migrate.ts to 100% the logic in the docs

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -2,15 +2,11 @@
  ? Migrate.ts is used to apply or "migrate" any changes made in schema.ts
  ? Remember to run the "generate" script before migrating
  */
-import { migrate } from "drizzle-orm/bun-sqlite/migrator";
-import db from "./db";
+ import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
-(async () => {
-    try {
-        await migrate(db, { migrationsFolder: "./drizzle" });
-        console.log("Migration successful")
-    } catch(error) {
-        console.error("Migration failed:", error)
-    }
-    
-})();
+ import { drizzle } from "drizzle-orm/bun-sqlite";
+ import { Database } from "bun:sqlite";
+ 
+ const sqlite = new Database("./database/db.sqlite");
+ const db = drizzle(sqlite);
+ migrate(db, { migrationsFolder: "./drizzle" });
