835882f98e2e1f34f9dafe81f1b3092a7efbdd8e
ErikMansen <mansen.erik@gmail.com>
2024-02-19 22:35:42 +0200

    Made migration logic more robust

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -2,8 +2,15 @@
  ? Migrate.ts is used to apply or "migrate" any changes made in schema.ts
  ? Remember to run the "generate" script before migrating
  */
-
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
-
 import db from "./db";
-migrate(db, { migrationsFolder: "./drizzle" });
+
+(async () => {
+    try {
+        await migrate(db, { migrationsFolder: "./drizzle" });
+        console.log("Migration successful")
+    } catch(error) {
+        console.error("Migration failed:", error)
+    }
+    
+})();
