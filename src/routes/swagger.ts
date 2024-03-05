0564c7cf21f081b79fc860fe9cb0c6af7c069865
ErikMansen <mansen.erik@gmail.com>
2024-03-05 16:17:40 +0200

    Corrected path to SwaggerUI

--- src/routes/swagger.ts
+++ src/routes/swagger.ts
@@ -1,84 +1,86 @@
-import { OpenAPIHono } from '@hono/zod-openapi'
-import { z } from '@hono/zod-openapi'
-import { createRoute } from '@hono/zod-openapi'
-import { swaggerUI } from '@hono/swagger-ui'
+import { OpenAPIHono, createRoute, z } from "@hono/zod-openapi";
+import { swaggerUI } from "@hono/swagger-ui";
 
 const ParamsSchema = z.object({
-    id: z.string().min(1).openapi({
-        param: {
-            name: 'id',
-            in: 'path',
-        },
-        example: '12',
-    }),
-})
+    id: z
+        .string()
+        .min(1)
+        .openapi({
+            param: {
+                name: "id",
+                in: "path",
+            },
+            example: "12",
+        }),
+});
 
 const HeadersSchema = z.object({
     // Header keys must be in lowercase, `Authorization` is not allowed.
     authorization: z.string().openapi({
-        example: 'TOKEN',
+        example: "TOKEN",
     }),
-})
+});
 
-const UserSchema = z.object({
-    id: z.string().openapi({
-        example: '123',
-    }),
-    username: z.string().openapi({
-        example: 'John Doe',
-    }),
-    password: z.string().min(8).openapi({
-        example: 'password123',
-    }),
-    admin: z.string().openapi({
-        example: 'False',
-    }),
-    token: z.string().openapi({
-
+const UserSchema = z
+    .object({
+        id: z.string().openapi({
+            example: "123",
+        }),
+        username: z.string().openapi({
+            example: "John Doe",
+        }),
+        password: z.string().min(8).openapi({
+            example: "password123",
+        }),
+        admin: z.string().openapi({
+            example: "False",
+        }),
+        token: z.string().openapi({}),
     })
-})
-    .openapi('User')
+    .openapi("User");
 
 const LoginSchema = z.object({
     message: z.string().openapi({
         example: "Login successful",
     }),
     token: z.string().openapi({
-        example: "Bearer TOKEN"
-    })
-})
-
-
+        example: "Bearer TOKEN",
+    }),
+});
 
 const userRoute = createRoute({
-    method: 'get',
-    path: '/user',
+    method: "get",
+    path: "/user",
     tags: ["User"],
-    security: [{
-        Bearer: [],
-    }],
+    security: [
+        {
+            Bearer: [],
+        },
+    ],
     request: {
         headers: HeadersSchema,
     },
     responses: {
         200: {
             content: {
-                'application/json': {
+                "application/json": {
                     schema: UserSchema,
                 },
             },
-            description: "Retrive all users"
-        }
-    }
-})
+            description: "Retrive all users",
+        },
+    },
+});
 
 const route = createRoute({
-    method: 'get',
-    path: '/user/{id}',
+    method: "get",
+    path: "/user/{id}",
     tags: ["User"],
-    security: [{
-        Bearer: [],
-    }],
+    security: [
+        {
+            Bearer: [],
+        },
+    ],
     request: {
         headers: HeadersSchema,
         params: ParamsSchema,
@@ -86,30 +88,30 @@ const route = createRoute({
     responses: {
         200: {
             content: {
-                'application/json': {
+                "application/json": {
                     schema: UserSchema,
                 },
             },
-            description: 'Retrieve the user',
+            description: "Retrieve the user",
         },
         401: {
-            description: 'Unauthorized'
-        }
+            description: "Unauthorized",
+        },
     },
-})
+});
 
 const login = createRoute({
-    method: 'post',
-    path: '/login',
+    method: "post",
+    path: "/login",
     tags: ["Login"],
     requestBody: {
         description: "Login as a user",
         content: {
-            'application/json': {
+            "application/json": {
                 example: {
                     username: "JohnDoe",
-                    passowrd: "password1234"
-                }
+                    passowrd: "password1234",
+                },
             },
         },
     },
@@ -117,100 +119,90 @@ const login = createRoute({
         200: {
             description: "Succesfull login",
             content: {
-                'application/json': {
-                    schema: LoginSchema
-                }
-            }
-        }
-    }
-})
+                "application/json": {
+                    schema: LoginSchema,
+                },
+            },
+        },
+    },
+});
 
 const register = createRoute({
-    method: 'post',
-    path: '/register',
-    tags: ['Register'],
+    method: "post",
+    path: "/register",
+    tags: ["Register"],
     requestBody: {
         description: "Register a new user",
         content: {
-            'application/json': {
+            "application/json": {
                 example: {
                     username: "JohnDoe",
-                    password: "password123"
-                }
-            }
-        }
+                    password: "password123",
+                },
+            },
+        },
     },
     responses: {
         200: {
             description: "Registration successful",
             content: {
-                'application/json': {
+                "application/json": {
                     schema: z.object({
                         message: z.string().openapi({
-                            example: "Registration successful"
-                        })
-                    })
+                            example: "Registration successful",
+                        }),
+                    }),
                 },
             },
         },
     },
-})
+});
 
-
-const app = new OpenAPIHono()
+const app = new OpenAPIHono();
 
 app.openAPIRegistry.registerComponent("securitySchemes", "Bearer", {
     type: "http",
     scheme: "bearer",
-})
+});
 app.openapi(register, (c) => {
     return c.json({
-        message: "message"
-    })
-})
-
+        message: "message",
+    });
+});
 
 app.openapi(login, (c) => {
     return c.json({
         message: "message",
-        token: "Token"
-    })
-})
-
+        token: "Token",
+    });
+});
 
 app.openapi(userRoute, (c) => {
     return c.json({
         id: "1",
-        username: 'Ultra-man',
-        password: 'password',
+        username: "Ultra-man",
+        password: "password",
         admin: "False",
-        token: "token"
-    })
-})
-
+        token: "token",
+    });
+});
 
 app.openapi(route, (c) => {
-    const { id } = c.req.valid('param')
+    const { id } = c.req.valid("param");
     return c.json({
         id,
-        username: 'Ultra-man',
-    })
-})
+        username: "Ultra-man",
+    });
+});
 // The OpenAPI documentation will be available at /doc
-app.doc('/doc', {
-    openapi: '3.0.0',
+app.doc("/doc", {
+    openapi: "3.0.0",
     info: {
-        version: '1.0.0',
-        title: 'User API',
+        version: "1.0.0",
+        title: "User API",
     },
-})
+});
 
+app.get("/ui", swaggerUI({ url: "/swagger/doc" }));
 
-app.get('/ui', swaggerUI({ url: 'http://localhost:3000/swagger/doc' }))
-
-export default app
-
-
-
-
-
+export default app;
